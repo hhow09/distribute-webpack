274d2a53f406844f02eee597e7047a3f76b0ee0a
hhow09 <hhow09@gmail.com>
2021-08-18 03:47:51 +0800

    initial

---
+++ const.js
@@ -0,0 +1,8 @@
+const workSpace = './ws';
+const repoName = 'webpack-code-split'
+const repoUrl = 'https://github.com/hhow09/webpack-code-split.git'
+module.exports = {
+    workSpace,
+    repoName,
+    repoUrl
+}
